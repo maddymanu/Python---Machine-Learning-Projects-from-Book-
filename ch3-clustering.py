47789a82bf0bca88a244c6c9c51ddc55a6b9cb9d
Aditya Bansal <maddymanu@hotmail.com>
2015-01-10 20:09:33 -0800

    trying more clustering.

--- ch3-clustering.py
+++ ch3-clustering.py
@@ -8,9 +8,20 @@ print vectorizer.get_feature_names()
 import os
 import sys
 
-from utils import DATA_DIR
+# from utils import DATA_DIR
 
-TOY_DIR = os.path.join(DATA_DIR, "toy")
+TOY_DIR = os.path.join(os.path.dirname(__file__), "toy")
 
 posts = [open(os.path.join(TOY_DIR , f)).read() for f in os.listdir(TOY_DIR)]
+posts.pop(0)
 print posts
+
+X_train = vectorizer.fit_transform(posts)
+num_samples , num_features = X_train.shape
+
+print num_samples
+
+new_post = "imaging databases"
+new_post_vec = vectorizer.transform([new_post])
+
+print new_post_vec
