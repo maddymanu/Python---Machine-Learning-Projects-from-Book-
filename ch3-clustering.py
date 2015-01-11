ee839ac9145b8b6a14492536743dc2727f9c0001
Aditya Bansal <maddymanu@hotmail.com>
2015-01-10 20:34:41 -0800

    Vectorization works!

--- ch3-clustering.py
+++ ch3-clustering.py
@@ -7,6 +7,7 @@ print vectorizer.get_feature_names()
 
 import os
 import sys
+import scipy as sp
 
 # from utils import DATA_DIR
 
@@ -24,4 +25,30 @@ print num_samples
 new_post = "imaging databases"
 new_post_vec = vectorizer.transform([new_post])
 
+
+def dist_raw(v1, v2):
+    delta = v1 - v2
+    return sp.linalg.norm(delta.toarray())
+
+
 print new_post_vec
+
+best_doc = None
+best_dist = sys.maxint
+best_i = None
+for i in range(0 , num_samples):
+	post = posts[i]
+	if post == new_post:
+		continue
+	post_vec = X_train.getrow(i)
+	d = dist_raw(post_vec , new_post_vec)
+	print ("===== Post %i with dist=%.2f: %s" %(i , d , post))
+	if d < best_dist:
+		best_dist = d
+		best_i = i
+
+print("Best post is %i with dist=%.2f" % (best_i, best_dist))
+
+
+
+
