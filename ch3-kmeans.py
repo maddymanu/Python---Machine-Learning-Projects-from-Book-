fc2ca7cf230040183009f0f27d975aafd258538e
Aditya Bansal <maddymanu@hotmail.com>
2015-01-11 17:03:36 -0800

    Finished extracting data.

--- ch3-kmeans.py
+++ ch3-kmeans.py
@@ -1,5 +1,10 @@
 import sklearn.datasets
+import ch3-clustering
 
 MLCOMP_DIR = r"/Users/adityabansal/Documents"
 data = sklearn.datasets.load_mlcomp("20news-18828", mlcomp_root=MLCOMP_DIR)
-print(data.filenames)
+
+groups = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.ma c.hardware', 'comp.windows.x', 'sci.space']
+train_data = sklearn.datasets.load_mlcomp("20news-18828", "train" , mlcomp_root=MLCOMP_DIR, categories=groups)
+
+print(len(train_data.filenames))
