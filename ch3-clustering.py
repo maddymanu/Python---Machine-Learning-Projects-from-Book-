96431d2242dd305824c9262b028585e3e0095e01
Aditya Bansal <maddymanu@hotmail.com>
2015-01-11 16:30:13 -0800

    Finished grouping words....starting KMeans.

--- ch3-clustering.py
+++ ch3-clustering.py
@@ -9,7 +9,6 @@ print vectorizer.get_feature_names()
 import os
 import sys
 import scipy as sp
-import nltk
 
 # from utils import DATA_DIR
 
@@ -17,22 +16,45 @@ TOY_DIR = os.path.join(os.path.dirname(__file__), "toy")
 
 posts = [open(os.path.join(TOY_DIR , f)).read() for f in os.listdir(TOY_DIR)]
 posts.pop(0)
-print posts
 
-X_train = vectorizer.fit_transform(posts)
-print X_train
-num_samples , num_features = X_train.shape
-
-print num_samples
 
 new_post = "imaging databases"
-new_post_vec = vectorizer.transform([new_post])
 
+
+import nltk.stem
+english_stemmer = nltk.stem.SnowballStemmer('english')
+
+class StemmedCountVectorizer(CountVectorizer):
+	def build_analyzer(self):
+		analyzer = super(StemmedCountVectorizer,self).build_analyzer()
+		return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
+
+vectorizer = StemmedCountVectorizer(min_df=1, stop_words='english')
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+class StemmedTfidfVectorizer(TfidfVectorizer):
+	def build_analyzer(self):
+		analyzer = super(TfidfVectorizer,self).build_analyzer()
+		return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
+
+
+vectorizer = StemmedTfidfVectorizer(min_df=1, stop_words='english', charset_error='ignore')
+X_train = vectorizer.fit_transform(posts)
+num_samples , num_features = X_train.shape
+print posts
+new_post_vec = vectorizer.transform([new_post])
 
 def dist_raw(v1, v2):
     delta = v1 - v2
     return sp.linalg.norm(delta.toarray())
 
+def dist_norm(v1, v2):
+    v1_normalized = v1 / sp.linalg.norm(v1.toarray())
+    v2_normalized = v2 / sp.linalg.norm(v2.toarray())
+
+    delta = v1_normalized - v2_normalized
+
+    return sp.linalg.norm(delta.toarray())
 
 best_doc = None
 best_dist = sys.maxint
@@ -42,7 +64,7 @@ for i in range(0 , num_samples):
 	if post == new_post:
 		continue
 	post_vec = X_train.getrow(i)
-	d = dist_raw(post_vec , new_post_vec)
+	d = dist_norm(post_vec , new_post_vec)
 	print ("===== Post %i with dist=%.2f: %s" %(i , d , post))
 	if d < best_dist:
 		best_dist = d
