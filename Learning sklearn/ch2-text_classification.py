5b8605545ee2563a5c9f5d337e633063ef3b1fff
Aditya Bansal <maddymanu@hotmail.com>
2015-01-18 15:31:42 -0800

    Metrics data works .

--- Learning sklearn/ch2-text_classification.py
+++ Learning sklearn/ch2-text_classification.py
@@ -56,28 +56,54 @@ def evaluate_cross_validation(clf, X, y, K):
 
 def get_stop_words():
 	result = set()
-	for line in open("data/stopwords)en.txt" , "r").readlines():
+	for line in open("data/stopwords_en.txt" , "r").readlines():
 		result.add(line.strip())
 		return result
 
 stop_words = get_stop_words()
-clf_5 = Pipeline([
+
+# clf_5 = Pipeline([
+#     ('vect', TfidfVectorizer(
+#                 stop_words=stop_words,
+#                 token_pattern=ur"\b[a-z0-9_\-\.]+[a-z][a-z0-9_\-\.]+\b",    
+#     )),
+#     ('clf', MultinomialNB()),
+# ])
+
+from sklearn import metrics
+
+
+clf_7 = Pipeline([
     ('vect', TfidfVectorizer(
                 stop_words=stop_words,
-                token_pattern=ur"\b[a-z0-9_\-\.]+[a-z][a-z0-9_\-\.]+\b",    
+                token_pattern=ur"\b[a-z0-9_\-\.]+[a-z][a-z0-9_\-\.]+\b",         
     )),
-    ('clf', MultinomialNB()),
+    ('clf', MultinomialNB(alpha=0.01)),
 ])
 
-evaluate_cross_validation(clf_5, news.data, news.target, 5)
+# evaluate_cross_validation(clf_7, news.data, news.target, 5)
 
 
 
+def train_and_evaluate(clf, X_train, X_test, y_train, y_test):
+    
+    clf.fit(X_train, y_train)
+    
+    print "Accuracy on training set:"
+    print clf.score(X_train, y_train)
+    print "Accuracy on testing set:"
+    print clf.score(X_test, y_test)
+    
+    y_pred = clf.predict(X_test)
+    
+    print "Classification Report:"
+    print metrics.classification_report(y_test, y_pred)
+    print "Confusion Matrix:"
+    print metrics.confusion_matrix(y_test, y_pred)
 
 
 
-
-
+train_and_evaluate(clf_7, X_train, X_test, y_train, y_test)
 
 
 
