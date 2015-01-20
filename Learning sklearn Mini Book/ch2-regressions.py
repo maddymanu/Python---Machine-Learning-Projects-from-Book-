ce497250f5454dad3690dcd46626d32c95a19a1d
Aditya Bansal <maddymanu@hotmail.com>
2015-01-19 20:36:34 -0800

    Data Extraction & Validation done.

---
+++ Learning sklearn Mini Book/ch2-regressions.py
@@ -0,0 +1,35 @@
+import IPython
+import sklearn as sk
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_boston
+
+boston = load_boston()
+print boston.data.shape
+
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(boston.data, boston.target, test_size=0.25, random_state=33)
+
+from sklearn.preprocessing import StandardScaler
+scalerX = StandardScaler().fit(X_train)
+scalery = StandardScaler().fit(y_train)
+X_train = scalerX.transform(X_train)
+y_train = scalery.transform(y_train)
+X_test = scalerX.transform(X_test)
+y_test = scalery.transform(y_test)
+
+
+from sklearn.cross_validation import *
+def train_and_evaluate(clf, X_train, y_train):
+    
+    clf.fit(X_train, y_train)
+    
+    print "Coefficient of determination on training set:",clf.score(X_train, y_train)
+    
+    # create a k-fold croos validation iterator of k=5 folds
+    cv = KFold(X_train.shape[0], 5, shuffle=True, random_state=33)
+    scores = cross_val_score(clf, X_train, y_train, cv=cv)
+    print "Average coefficient of determination using 5-fold crossvalidation:",np.mean(scores)
