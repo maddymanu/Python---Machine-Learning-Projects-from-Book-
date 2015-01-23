90c8ab4c5b43a2ec210f7e167a00c2191758d977
Aditya Bansal <maddymanu@hotmail.com>
2015-01-22 20:29:39 -0800

    Completed KNN.

--- Website/k_nearest_neighbour_classification.py
+++ Website/k_nearest_neighbour_classification.py
@@ -11,3 +11,21 @@ x = iris.data[: , :2]
 y = iris.target
 
 print x.shape
+
+h = 0.02
+
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+for weights in ['uniform' , 'distance']:
+	clf = neighbors.KNeighborsClassifier(n_neigh , weights = weights)
+
+	x_min = x[: , 0].min()-1
+	x_max = x[: , 0].max()+1
+
+	y_min = x[: , 1].min()-1
+	y_max = x[: , 1].max()+1
+
+	xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
