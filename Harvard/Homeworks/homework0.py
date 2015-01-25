83d2e240ea1a9081972db855d07028649dfb4f18
Aditya Bansal <maddymanu@hotmail.com>
2015-01-24 19:03:08 -0800

    Completed hw0.

--- Harvard/Homeworks/homework0.py
+++ Harvard/Homeworks/homework0.py
@@ -13,3 +13,56 @@ print x.max(axis=1)
 print x.min()
 print x.mean()
 
+"""
+Function
+--------
+simulate_prizedoor
+
+Generate a random array of 0s, 1s, and 2s, representing
+hiding a prize between door 0, door 1, and door 2
+
+Parameters
+----------
+nsim : int
+    The number of simulations to run
+
+Returns
+-------
+sims : array
+    Random array of 0s, 1s, and 2s
+
+Example
+-------
+>>> print simulate_prizedoor(3)
+array([0, 0, 2])
+"""
+def simulate_prizedoor(nsim):
+    answer = np.random.randint(0,3, nsim)
+    print answer
+    return answer
+#your code here
+
+# simulate_prizedoor(3)
+
+def simulate_guess(nsim):
+	ans = np.zeros(nsim, dtype=np.int)
+	print ans
+	return ans
+
+
+simulate_guess(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# end
+
