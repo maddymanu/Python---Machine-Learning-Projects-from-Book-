9d998ff34ccdc4dbf859d35150ff9dcc966d8d83
Aditya Bansal <maddymanu@hotmail.com>
2015-01-27 18:12:42 -0800

    started hw2. p1.

---
+++ Harvard/Homeworks/homework2.py
@@ -0,0 +1,18 @@
+import requests 
+import StringIO 
+import numpy as np
+import pandas as pd # pandas
+import matplotlib.pyplot as plt # module for plotting 
+import datetime as dt # module for manipulating dates and times
+import numpy.linalg as lin # module for performing linear algebra operations
+
+url_exprs = "https://raw.githubusercontent.com/cs109/2014_data/master/exprs_GSE5859.csv"
+exprs = pd.read_csv(url_exprs, index_col=0)
+
+url_sampleinfo = "https://raw.githubusercontent.com/cs109/2014_data/master/sampleinfo_GSE5859.csv"
+sampleinfo = pd.read_csv(url_sampleinfo)
+
+# print exprs.columns
+# print sampleinfo.filename
+
+print sampleinfo[exprs.columns == sampleinfo.filename]
