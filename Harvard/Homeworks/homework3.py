50c01697632d930760f070693c8db1145749c134
Aditya Bansal <maddymanu@hotmail.com>
2015-01-28 19:01:22 -0800

    Split the data. Starting Dimensioanlity reduction.

--- Harvard/Homeworks/homework3.py
+++ Harvard/Homeworks/homework3.py
@@ -65,7 +65,9 @@ Y = iris.target
 
 print X.shape, Y.shape
 
+from sklearn.cross_validation import train_test_split
 
+x_train , x_test , y_train, y_test = train_test_split(X , Y , test_size=0.33 , random_state=42)
 
 
 
